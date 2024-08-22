5a003658654718a59effd9e4c980e9908d27d07c
Waldek Bira <waldek.bira@gmail.com>
2024-08-23 00:38:55 +0200

    Improved solution and tests

--- solutions/arrays/twosum/twosum_test.go
+++ solutions/arrays/twosum/twosum_test.go
@@ -1,6 +1,10 @@
 package twosum
 
-import "testing"
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
 
 func TestTwoSum(t *testing.T) {
 	tests := []struct {
@@ -11,11 +15,17 @@ func TestTwoSum(t *testing.T) {
 		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
 		{[]int{3, 2, 4}, 6, []int{1, 2}},
 		{[]int{3, 3}, 6, []int{0, 1}},
+		{[]int{3, 2, 3}, 6, []int{0, 2}}, // Additional test case
 	}
 
 	for _, tt := range tests {
 		got := TwoSum(tt.nums, tt.target)
-		if got[0] != tt.want[0] || got[1] != tt.want[1] {
+
+		// Sort both slices to ensure order doesn't matter
+		sort.Ints(got)
+		sort.Ints(tt.want)
+
+		if !reflect.DeepEqual(got, tt.want) {
 			t.Errorf("TwoSum(%v, %v) = %v; want %v", tt.nums, tt.target, got, tt.want)
 		}
 	}
