1302d89353b091be9a13835f7a672943437f5de1
Waldek Bira <waldek.bira@gmail.com>
2024-09-04 22:36:07 +0200

    Implemented merge two storted linked list

--- solutions/arrays/merge_two_sorted_list/merge.go
+++ solutions/arrays/merge_two_sorted_list/merge.go
@@ -6,19 +6,41 @@ type ListNode struct {
 }
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	// if list1 == nil {
+	// 	return list2
+	// }
+
+	// if list2 == nil {
+	// 	return list1
+	// }
+
+	// if list1.Val <= list2.Val {
+	// 	list1.Next = mergeTwoLists(list1.Next, list2)
+	// 	return list1
+	// } else {
+	// 	list2.Next = mergeTwoLists(list1, list2.Next)
+	// 	return list2
+	// }
+	head := &ListNode{}
+	current := head
+
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+		current = current.Next
+	}
+
 	if list1 == nil {
-		return list2
-	}
-
-	if list2 == nil {
-		return list1
-	}
-
-	if list1.Val <= list2.Val {
-		list1.Next = mergeTwoLists(list1.Next, list2)
-		return list1
+		current.Next = list2
 	} else {
-		list2.Next = mergeTwoLists(list1, list2.Next)
-		return list2
+		current.Next = list1
 	}
+
+	return head.Next
+
 }
